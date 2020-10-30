edb543b69144d1c3645b5b327cd756f5d37316d7
enisazizi <enisazizi@hotmail.com>
2020-10-30 14:49:49 +0100

    teamwork

--- artist.js
+++ artist.js
@@ -47,3 +47,22 @@ function linkBorder3(){
 
         
 }
+function myFunction() {
+    // Declare variables
+    let input, filter, ul, li, a,  txtValue;
+    input = document.querySelector('#myInput');
+    filter = input.value.toUpperCase();
+    ul = document.querySelector("#myUL");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for ( let i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
